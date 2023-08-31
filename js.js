e8974fc95fcda3e2b8ffba7964299fb4fb3f581c
Mason Clark <109944632+MasonClark01@users.noreply.github.com>
2023-08-30 17:45:22 -0700

    Added image changing functionality.

--- js.js
+++ js.js
@@ -1,14 +1,57 @@
-let rps = [1, 1, 1]
-let round = 0
-let previousChoice = []
+let pscore = 0
+let bscore = 0
 
-function rpsRound(playerInput){
-    let x = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-    if(playerInput === 2 && x === 0){
-        return false
+let previousChoice = undefined
+let bobChoices = []
+
+function changePlayerImg(image){
+    if(image === 0){
+        document.getElementById("player-img").src = "./images/rock.svg"
     }
-    else{
-        return(playerInput > x)
+    if(image === 1){
+        document.getElementById("player-img").src = "./images/paper.svg"
+    }
+    if(image === 2){
+        document.getElementById("player-img").src = "./images/scissors.svg"
     }
 }
 
+function changeText(control){
+    if(x = 0){
+
+    }
+    //1 for player win, -1 for bobwin, 0 for tie. update pscore and bscore here and update score id and text id.
+}
+function rpsRound(playerInput){
+    let x = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    let pI = playerInput
+    bobChoices.push(x);
+    if(previousChoice === undefined){
+        bobChoices.push(Math.floor(Math.random() * (2 - 0 + 1)));
+    }
+    else{
+        if(previousChoice === 2){
+            bobChoices.push(0);
+        }
+        if(previousChoice === 1){
+            bobChoices.push(2)
+        }
+        if(previousChoice === 0){
+            bobChoices.push(1)
+        }
+    }
+    console.log(bobChoices)
+    let bobRound = bobChoices[Math.floor(Math.random() * (bobChoices.length - 0 + 1))];
+    console.log(bobRound)
+    previousChoice = playerInput
+    changePlayerImg(playerInput)
+    if((playerInput === 2 && bobRound === 0) || (playerInput === 0 && bobRound === 1)){
+        changeText(-1)
+    }
+    else if(playerInput === bobRound){
+        changeText(0)
+    }
+    else{
+        changeText(1)
+    }
+}
